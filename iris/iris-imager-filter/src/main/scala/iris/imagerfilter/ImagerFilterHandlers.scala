897975f564e0bb23a5acd0ae78e90c9d1341617d
Shubham Chauhan <shubham25may1998@gmail.com>
2021-09-14 12:38:49 +0530

    #000: Chauhan: Implement fuctionality to process wheel1 command in ImagerFilterHandlers

--- iris/iris-imager-filter/src/main/scala/iris/imagerfilter/ImagerFilterHandlers.scala
+++ iris/iris-imager-filter/src/main/scala/iris/imagerfilter/ImagerFilterHandlers.scala
@@ -1,16 +1,15 @@
 package iris.imagerfilter
 
-import akka.actor.typed.scaladsl.ActorContext
+import akka.actor.typed.scaladsl.{ActorContext, Behaviors}
+import akka.actor.typed.{ActorRef, Behavior}
 import csw.command.client.messages.TopLevelActorMessage
 import csw.framework.models.CswContext
 import csw.framework.scaladsl.ComponentHandlers
 import csw.location.api.models.TrackingEvent
 import csw.params.commands.CommandResponse._
-import csw.params.commands.ControlCommand
+import csw.params.commands.{CommandIssue, ControlCommand}
 import csw.params.core.generics.Parameter
 import csw.params.core.models.Id
-import csw.params.events.{EventName, SystemEvent}
-import csw.prefix.models.Prefix
 import csw.time.core.models.UTCTime
 
 import scala.concurrent.ExecutionContextExecutor
@@ -28,17 +27,11 @@ class ImagerFilterHandlers(ctx: ActorContext[TopLevelActorMessage], cswCtx: CswC
   import cswCtx._
   implicit val ec: ExecutionContextExecutor = ctx.executionContext
   private val log                           = loggerFactory.getLogger
-  private var currentPosition               = "f1"
-  private val positions                     = List("f1", "f2", "f3", "f4", "f5", "f6", "f7", "f8", "f9", "f10", "f11", "f12", "f13", "f14", "f15", "f16")
+  private val imageActor                    = ctx.spawnAnonymous(FilterWheelActor.behavior(cswCtx))
+
   override def initialize(): Unit = {
     log.info("Initializing imager.filter...")
 
-  }
-
-  def getMoves(current: String, target: String): List[String] = {
-    val currentIndex = positions.indexOf(current)
-    val targetIndex  = positions.indexOf(target)
-    positions.slice(currentIndex + 1, targetIndex + 1)
   }
 
   override def onLocationTrackingEvent(trackingEvent: TrackingEvent): Unit = {}
@@ -46,38 +39,14 @@ class ImagerFilterHandlers(ctx: ActorContext[TopLevelActorMessage], cswCtx: CswC
   override def validateCommand(runId: Id, controlCommand: ControlCommand): ValidateCommandResponse = Accepted(runId)
 
   override def onSubmit(runId: Id, controlCommand: ControlCommand): SubmitResponse = {
-//    val parameter: Parameter[String] = StringKey.make("wheel1").set("f1")
-    val parameter: Parameter[_] = controlCommand.paramSet.find(_.keyName == "wheel1").get
-    parameter.values.head match {
-      case s: String => {
-        val moves = getMoves(currentPosition, s)
+    val parameter         = controlCommand.paramSet.find(_.keyName == "wheel1").get.asInstanceOf[Parameter[String]]
+    val targetPos: String = parameter.values.head
 
-      }
+    if (FilterWheelActor.positions.contains(targetPos)) {
+      imageActor ! FilterWheelActorMessage.Wheel1(targetPos)
+      Started(runId)
     }
-
-    //f1, f4
-
-    //f1,f2
-    //f2,f3
-    //f3,f4
-    //f4,f4
-
-    (currentPosition, parameter.values.head) match {
-      case ("f1", "f2") | ("f2", "f1") =>
-      case ("f1", "f2")                =>
-      case ("f1", "f2")                =>
-    }
-
-    parameter.values.head match {
-      case s: String =>
-        println(s)
-        currentPosition = s
-        val event = SystemEvent(Prefix("IRIS.imager.filter"), EventName("filterWheel"))
-        eventService.defaultPublisher.publish(event.madd(parameter))
-    }
-    Started(runId)
-    //sleep
-    Completed(runId)
+    else Invalid(runId, CommandIssue.ParameterValueOutOfRangeIssue(s"$targetPos is not a valid position"))
   }
 
   override def onOneway(runId: Id, controlCommand: ControlCommand): Unit = {}
@@ -92,4 +61,56 @@ class ImagerFilterHandlers(ctx: ActorContext[TopLevelActorMessage], cswCtx: CswC
 
   override def onOperationsMode(): Unit = {}
 
+}
+
+class FilterWheelActor(cswContext: CswContext) {
+
+  private def getCurrentIndexAndDiff(currentPos: String, targetPos: String): (Int, Int) = {
+    val currentPosIndex = FilterWheelActor.positions.indexOf(currentPos)
+    val targetPosIndex  = FilterWheelActor.positions.indexOf(targetPos)
+    if (targetPosIndex > currentPosIndex) (currentPosIndex, +1) else (currentPosIndex, -1)
+  }
+
+  private def move(
+      currentPosIndex: Int,
+      diff: Int,
+      targetPos: String,
+      intervalInMS: Int = 500,
+      self: ActorRef[FilterWheelActorMessage]
+  ): Behavior[FilterWheelActorMessage] = {
+    if (currentPosIndex < 0 || FilterWheelActor.positions(currentPosIndex) == targetPos) return Behaviors.same
+
+    val nextPosIndex = currentPosIndex + diff
+    cswContext.timeServiceScheduler.scheduleOnce(UTCTime(UTCTime.now().value.plusMillis(intervalInMS))) {
+      self ! FilterWheelActorMessage.Move(nextPosIndex, diff, targetPos, intervalInMS)
+    }
+    behavior(FilterWheelActor.positions(nextPosIndex))
+  }
+
+  def behavior(currentPos: String): Behavior[FilterWheelActorMessage] = {
+    Behaviors.receive { (ctx, msg) =>
+      msg match {
+        case FilterWheelActorMessage.Wheel1(target) =>
+          val (currentIndex, diff) = getCurrentIndexAndDiff(currentPos, target)
+          move(currentIndex, diff, target, 500, ctx.self)
+
+        case FilterWheelActorMessage.Move(currentIndex, diff, targetPos, intervalInMS) =>
+          move(currentIndex, diff, targetPos, intervalInMS, ctx.self)
+      }
+    }
+  }
+}
+
+trait FilterWheelActorMessage
+
+object FilterWheelActorMessage {
+  case class Wheel1(target: String)                                                   extends FilterWheelActorMessage
+  case class Move(currentIndex: Int, diff: Int, targetPos: String, intervalInMS: Int) extends FilterWheelActorMessage
+}
+
+object FilterWheelActor {
+  val positions       = List("f1", "f2", "f3", "f4", "f5", "f6", "f7", "f8", "f9", "f10", "f11", "f12", "f13", "f14", "f15", "f16")
+  val initialPosition = "f1"
+
+  def behavior(cswContext: CswContext): Behavior[FilterWheelActorMessage] = new FilterWheelActor(cswContext).behavior(initialPosition)
 }
