320156fdb0c9d592dc192ee099d68e0186e8de8f
Himanshu Mertia <22441818+mertia-himanshu@users.noreply.githhub.com>
2021-10-25 15:07:16 +0530

    #ESW-000: Himanshu | Manish: correct wfos path in jitpack

--- publish.sh
+++ publish.sh
@@ -4,6 +4,6 @@ cd iris || exit
 
 java -Xms2048m -Xmx2048m -XX:ReservedCodeCacheSize=512m -jar ../sbt-launch-1.5.5.jar -Dsbt.log.noformat=true -Dprod.publish=true clean publishM2
 
-cd wfos || exit
+cd ../wfos || exit
 
 java -Xms2048m -Xmx2048m -XX:ReservedCodeCacheSize=512m -jar ../sbt-launch-1.5.5.jar -Dsbt.log.noformat=true -Dprod.publish=true clean publishM2
