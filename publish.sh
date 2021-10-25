f71be58ff9f77fe2c8496a7ff1a499f4600d1f1b
Himanshu Mertia <22441818+mertia-himanshu@users.noreply.githhub.com>
2021-10-25 15:04:11 +0530

    #ESW-000: Himanshu | Manish: Add jitpack for publishing

---
+++ publish.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+cd iris || exit
+
+java -Xms2048m -Xmx2048m -XX:ReservedCodeCacheSize=512m -jar ../sbt-launch-1.5.5.jar -Dsbt.log.noformat=true -Dprod.publish=true clean publishM2
+
+cd wfos || exit
+
+java -Xms2048m -Xmx2048m -XX:ReservedCodeCacheSize=512m -jar ../sbt-launch-1.5.5.jar -Dsbt.log.noformat=true -Dprod.publish=true clean publishM2
