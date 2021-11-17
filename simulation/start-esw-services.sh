0d143e0f43aaaf4f2908eb85cc5417a3affa2351
manish <gowardipemanish95@gmail.com>
2021-11-17 21:32:51 +0530

    #ESW-000 :Manish: Update scripts sha & fix jsons

--- simulation/start-esw-services.sh
+++ simulation/start-esw-services.sh
@@ -4,5 +4,5 @@ ROOT="$(
     pwd -P
 )"
 ESW_VERSION=bb819c0
-SEQ_SCRIPT_VERSION=4756c1b
+SEQ_SCRIPT_VERSION=81de9b1
 cs launch esw-services:$ESW_VERSION -- start-eng-ui-services --scripts-version $SEQ_SCRIPT_VERSION --obs-mode-config $ROOT/../sample-configs/smObsModeConfig.conf
