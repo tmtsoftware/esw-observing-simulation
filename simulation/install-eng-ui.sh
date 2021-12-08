ef82733edfd76aba48bf087e05f44d0f63f1f11c
manish <gowardipemanish95@gmail.com>
2021-12-08 09:41:26 +0530

    #ESW-561: Manish : Update esw & scripts sha which supports multiple sequencers. eng-ui script to download latest code & setup

--- simulation/install-eng-ui.sh
+++ simulation/install-eng-ui.sh
@@ -4,5 +4,16 @@ ROOT="$(
     pwd -P
 )"
 cd $ROOT/../apps
-curl -L -O https://github.com/tmtsoftware/esw-ocs-eng-ui/releases/download/v0.1.0/esw-ocs-eng-ui.zip
-unzip -o esw-ocs-eng-ui.zip
+
+# use this for installing released version
+#curl -L -O https://github.com/tmtsoftware/esw-ocs-eng-ui/releases/download/v0.1.0/esw-ocs-eng-ui.zip
+#unzip -o esw-ocs-eng-ui.zip
+
+
+## use following for installing latest eng-ui
+curl -L -O https://github.com/tmtsoftware/esw-ocs-eng-ui/archive/refs/heads/main.zip
+unzip -o main.zip
+cd esw-ocs-eng-ui-main && npm install && npm run build
+mv esw-ocs-eng-ui ../esw-ocs-eng-ui
+cd ..
+rm -rf esw-ocs-eng-ui-main
--- simulation/start-esw-services.sh
+++ simulation/start-esw-services.sh
@@ -3,6 +3,6 @@ ROOT="$(
     cd "$(dirname "$0")" >/dev/null 2>&1 || exit
     pwd -P
 )"
-ESW_VERSION=bb819c0
-SEQ_SCRIPT_VERSION=b3d5d1e
-cs launch esw-services:$ESW_VERSION -- start-eng-ui-services --scripts-version $SEQ_SCRIPT_VERSION --obs-mode-config $ROOT/../sample-configs/smObsModeConfig.conf
+ESW_VERSION=cd601b9
+SEQ_SCRIPT_VERSION=a145d79
+cs launch esw-services:$ESW_VERSION -- start-eng-ui-services  --scripts-version $SEQ_SCRIPT_VERSION --obs-mode-config $ROOT/../sample-configs/smObsModeConfig.conf
