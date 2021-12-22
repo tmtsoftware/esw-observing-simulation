554ccd06c56fdfc617b8e75152fd9bea1307464e
Himanshu Mertia <22441818+mertia-himanshu@users.noreply.githhub.com>
2021-12-22 16:48:48 +0530

    #ESW-569 : Manish | Himanshu : Try running install.sh to install os specific libs

--- integration/src/test/resources/test-setup.sh
+++ integration/src/test/resources/test-setup.sh
@@ -15,17 +15,28 @@ ROOT="$(
     cd "$(dirname "$0")" >/dev/null 2>&1 || exit
     pwd -P
 )"
-cd $ROOT/../../../target
 
-V_SLICE_ZIP=https://github.com/tmtsoftware/tcs-vslice-0.4/releases/download/v0.4/tcs-vslice-04.zip
-CONTAINER_CONF_PATH=$1
+if [[ "$OSTYPE" == "darwin"* ]]; then
 
-if [ -d "tcs-vslice-04" ]
-then
-    echo "starting tcs-vslice container"
+  cd $ROOT/../../../target
+
+  V_SLICE_ZIP=https://github.com/tmtsoftware/tcs-vslice-0.4/releases/download/v0.4/tcs-vslice-04.zip
+  CONTAINER_CONF_PATH=$1
+
+  if [ -d "tcs-vslice-04" ]
+  then
+      echo "starting tcs-vslice container"
+  else
+     curl -L -O $V_SLICE_ZIP
+     unzip -o tcs-vslice-04
+  fi
+
+  tcs-vslice-04/bin/tcs-deploy --local $CONTAINER_CONF_PATH
+
 else
-   curl -L -O $V_SLICE_ZIP
-   unzip -o tcs-vslice-04
+  echo $ROOT
+  cd $ROOT/../../../../../tcs-vslice-0.4
+  echo "after switching to tcs vslice folder"
+  echo pwd
+  ./install/tcs-vslice-04/bin/tcs-deploy --local $CONTAINER_CONF_PATH
 fi
-
-tcs-vslice-04/bin/tcs-deploy --local $CONTAINER_CONF_PATH
