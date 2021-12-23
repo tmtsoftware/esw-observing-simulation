6ffc37edb420b22e7317efcc587c992b2d860115
Himanshu Mertia <22441818+mertia-himanshu@users.noreply.githhub.com>
2021-12-23 09:50:58 +0530

    #ESW-569 : Manish | Himanshu : Use os specific binaries for ubuntu 20.04

--- integration/src/test/resources/test-setup.sh
+++ integration/src/test/resources/test-setup.sh
@@ -3,9 +3,13 @@
 if [[ "$OSTYPE" == "darwin"* ]]; then
   export INTERFACE_NAME=en0
   export AAS_INTERFACE_NAME=en0
+  V_SLICE_ZIP=https://github.com/tmtsoftware/tcs-vslice-0.4/releases/download/v0.4/tcs-vslice-04.zip
+
 else
   export INTERFACE_NAME=eth0
   export AAS_INTERFACE_NAME=eth0
+  V_SLICE_ZIP=https://github.com/tmtsoftware/tcs-vslice-0.4/releases/download/v0.4/tcs-vslice-04-Ubuntu-20.04.zip
+
 fi
 
 echo "INTERFACE_NAME:"$INTERFACE_NAME
@@ -18,26 +22,16 @@ ROOT="$(
 
 CONTAINER_CONF_PATH=$1
 
-if [[ "$OSTYPE" == "darwin"* ]]; then
 
-  cd $ROOT/../../../target
+cd $ROOT/../../../target
 
-  V_SLICE_ZIP=https://github.com/tmtsoftware/tcs-vslice-0.4/releases/download/v0.4/tcs-vslice-04.zip
-
-
-  if [ -d "tcs-vslice-04" ]
-  then
-      echo "starting tcs-vslice container"
-  else
-     curl -L -O $V_SLICE_ZIP
-     unzip -o tcs-vslice-04
-  fi
-
-  tcs-vslice-04/bin/tcs-deploy --local $CONTAINER_CONF_PATH
-
+if [ -d "tcs-vslice-04" ]
+then
+    echo "starting tcs-vslice container"
 else
-  echo $ROOT
-  cd $ROOT/../../../../../tcs-vslice-0.4
-  echo "after switching to tcs vslice folder ${PWD}"
-  ./install/tcs-vslice-04/bin/tcs-deploy --local $CONTAINER_CONF_PATH
+   echo "downloading.." $V_SLICE_ZIP
+   curl -L -O $V_SLICE_ZIP -o tcs-vslice-04.zip
+   unzip -o tcs-vslice-04.zip
 fi
+
+tcs-vslice-04/bin/tcs-deploy --local $CONTAINER_CONF_PATH
