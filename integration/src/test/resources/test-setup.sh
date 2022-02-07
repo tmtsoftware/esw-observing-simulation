6a291c52586ff8d61bba7aeb0eb94e704a0c33f1
manish <gowardipemanish95@gmail.com>
2022-02-07 13:13:31 +0530

    #ESW-000: Manish | Himanshu : Remove setting of INTERFACE_NAME in scripts

--- integration/src/test/resources/test-setup.sh
+++ integration/src/test/resources/test-setup.sh
@@ -1,11 +1,9 @@
 #!/usr/bin/env bash
 
 if [[ "$OSTYPE" == "darwin"* ]]; then
-  export INTERFACE_NAME=en0
   V_SLICE_ZIP=https://github.com/tmtsoftware/tcs-vslice-0.4/releases/download/v0.7/tcs-vslice-0.7.zip
 
 else
-  export INTERFACE_NAME=eth0
   V_SLICE_ZIP=https://github.com/tmtsoftware/tcs-vslice-0.4/releases/download/v0.7/tcs-vslice-07-Ubuntu-20.04.zip
 
 fi
--- simulation-scripts/start-tcs-assemblies.sh
+++ simulation-scripts/start-tcs-assemblies.sh
@@ -1,13 +1,5 @@
 #!/usr/bin/env bash
 
-if [[ "$OSTYPE" == "darwin"* ]]; then
-  export INTERFACE_NAME=en0
-else
-  export INTERFACE_NAME=eth0
-fi
-
-echo "Setting INTERFACE_NAME tcs assemblies:"$INTERFACE_NAME
-
 export TPK_USE_FAKE_SYSTEM_CLOCK=1
 
 ROOT="$(
