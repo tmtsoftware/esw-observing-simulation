605566a45345bd52e2198f59065f607c3c8283c1
manish <gowardipemanish95@gmail.com>
2022-03-29 13:33:29 +0530

    #ESW-000 : Himanshu | Manish : Start eng-ui by using specific commit SHA in simulation

--- simulation-scripts/install-eng-ui.sh
+++ simulation-scripts/install-eng-ui.sh
@@ -3,14 +3,27 @@ ROOT="$(
     cd "$(dirname "$0")" >/dev/null 2>&1 || exit
     pwd -P
 )"
+cd $ROOT
+source ./versions.sh
+version=$ENG_UI_VERSION
+
 cd $ROOT/../apps
+
+rm -r *.txt
+rm -r *.zip
 
 #curl -L -O https://github.com/tmtsoftware/esw-ocs-eng-ui/releases/download/v0.2.0/esw-ocs-eng-ui.zip
 #unzip -o esw-ocs-eng-ui.zip
 ## use following for installing latest eng-ui
-curl -L -O https://github.com/tmtsoftware/esw-ocs-eng-ui/archive/refs/heads/main.zip
-unzip -o main.zip
-cd esw-ocs-eng-ui-main && npm install && npm run build
+
+ENG_UI_HOME="$ROOT/../apps/"
+
+echo "downloading.. esw-ocs-eng-ui with $version"
+curl -L -O https://github.com/tmtsoftware/esw-ocs-eng-ui/archive/$version.zip
+
+unzip -o $version.zip
+cd esw-ocs-eng-ui-$version* && npm install && npm run build
 mv esw-ocs-eng-ui ../esw-ocs-eng-ui
 cd ..
-rm -rf esw-ocs-eng-ui-main
+rm -rf esw-ocs-eng-ui-$version*
+touch $version.txt
--- simulation-scripts/start-eng-ui.sh
+++ simulation-scripts/start-eng-ui.sh
@@ -3,8 +3,12 @@ ROOT="$(
     cd "$(dirname "$0")" >/dev/null 2>&1 || exit
     pwd -P
 )"
+cd $ROOT
+source ./versions.sh
+version=$ENG_UI_VERSION
 cd $ROOT/../apps
-if [ -d "esw-ocs-eng-ui" ]
+
+if [ -f "$version.txt" ]
 then
     echo "Serving esw-ocs-eng-ui app "
 else
