50f67036ffb55c639ae9dfaedf3348a3995b303f
Allan Brighton <Allan.Brighton@t-online.de>
2025-02-26 14:15:16 +0100

    Updating dependencies

---
+++ integration/src/test/resources/test-setup-xxx.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  export INTERFACE_NAME=en0
+  V_SLICE_ZIP=https://github.com/tmtsoftware/tcs-vslice-0.4/releases/download/v0.7/tcs-vslice-0.7.zip
+
+#else
+#  export INTERFACE_NAME=eth0
+#  V_SLICE_ZIP=https://github.com/tmtsoftware/tcs-vslice-0.4/releases/download/v0.6/tcs-vslice-06-Ubuntu-20.04.zip
+
+fi
+
+#echo "Setting INTERFACE_NAME tcs assemblies:"$INTERFACE_NAME
+
+export TPK_USE_FAKE_SYSTEM_CLOCK=1
+
+ROOT="$(
+    cd "$(dirname "$0")" >/dev/null 2>&1 || exit
+    pwd -P
+)"
+
+CONTAINER_CONF_PATH=$1
+
+
+cd $ROOT/../../../target
+
+#if [ -d "tcs-vslice-04" ]
+#then
+#    echo "starting tcs-vslice container"
+#else
+#   echo "downloading.." $V_SLICE_ZIP
+#   curl -L $V_SLICE_ZIP -o tcs-vslice-04.zip
+#   unzip -o tcs-vslice-04.zip
+#fi
+
+/shared/work/tmt/csw/tcs-vslice-0.4/target/universal/stage/bin/tcs-deploy --local $CONTAINER_CONF_PATH
