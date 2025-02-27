fcf88b8399755b872674f0ef566be006e64b3d46
Allan Brighton <Allan.Brighton@t-online.de>
2025-02-27 16:29:37 +0100

    Updating dependencies

--- integration/src/test/scala/esw/observing/simulation/ScriptVersion.scala
+++ integration/src/test/scala/esw/observing/simulation/ScriptVersion.scala
@@ -2,5 +2,6 @@ package esw.observing.simulation
 
 object ScriptVersion {
   // This is the version (or Git SHA) of the version of the sequencer-scripts repo to use
-  val value = "31a03cd"
+  val value = "204f3e4"
+//  val value = "0.1.0-SNAPSHOT"
 }
--- simulation-scripts/versions.sh
+++ simulation-scripts/versions.sh
@@ -2,7 +2,7 @@
 
 export ESW_VERSION=4d97ed9
 export CSW_VERSION=2f05249
-export SEQ_SCRIPT_VERSION=a818829
+export SEQ_SCRIPT_VERSION=204f3e4
 export TCS_VERSION=49d6af7
 export ENG_UI_VERSION=82b542e
 export CHANNEL="https://raw.githubusercontent.com/tmtsoftware/osw-apps/Allan/pekko-scala3-update/apps.json"
