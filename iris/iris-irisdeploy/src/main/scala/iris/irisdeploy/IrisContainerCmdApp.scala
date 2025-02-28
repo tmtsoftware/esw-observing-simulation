87655c9f0aef61e39e57ba9bb889e1ff515bed93
Allan Brighton <Allan.Brighton@t-online.de>
2025-02-28 11:36:57 +0100

    Updating dependencies

--- iris/iris-irisdeploy/src/main/scala/iris/irisdeploy/IrisContainerCmdApp.scala
+++ iris/iris-irisdeploy/src/main/scala/iris/irisdeploy/IrisContainerCmdApp.scala
@@ -3,6 +3,8 @@ package iris.irisdeploy
 import csw.framework.deploy.containercmd.ContainerCmd
 import csw.prefix.models.Subsystem.IRIS
 
-object IrisContainerCmdApp extends App {
-  ContainerCmd.start("iris_container_cmd_app", IRIS, args)
+object IrisContainerCmdApp {
+  def main(args: Array[String]): Unit = {
+    ContainerCmd.start("iris_container_cmd_app", IRIS, args)
+  }
 }
--- wfos/wfos-wfosdeploy/src/main/scala/wfos/wfosdeploy/WfosContainerCmdApp.scala
+++ wfos/wfos-wfosdeploy/src/main/scala/wfos/wfosdeploy/WfosContainerCmdApp.scala
@@ -3,6 +3,8 @@ package wfos.wfosdeploy
 import csw.framework.deploy.containercmd.ContainerCmd
 import csw.prefix.models.Subsystem.WFOS
 
-object WfosContainerCmdApp extends App {
-  ContainerCmd.start("wfos_container_cmd_app", WFOS, args)
+object WfosContainerCmdApp {
+  def main(args: Array[String]): Unit = {
+    ContainerCmd.start("wfos_container_cmd_app", WFOS, args)
+  }
 }
