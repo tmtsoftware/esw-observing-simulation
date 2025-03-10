d66b002ba4a2fd74a1cce32a2ea5aaa4800c1035
Allan Brighton <Allan.Brighton@t-online.de>
2025-03-10 18:17:38 +0100

    Testing Jenkins setup

--- integration/xxx.sh
+++ integration/xxx.sh
@@ -4,7 +4,11 @@
 # This script tries to shutdown any redis server.
 
 set -x
-redis-cli shutdown || echo "XXX: Redis was not running"
-kill `lsof -t -i:6379`
-kill `lsof -t -i:26379`
-exit 0
+#redis-cli shutdown || echo "XXX: Redis was not running"
+#kill `lsof -t -i:6379`
+#kill `lsof -t -i:26379`
+#exit 0
+
+ps -efl | grep 6379 | grep -v grep | awk '{ print $4 }' | xargs kill
+ps -efl | grep 26379 | grep -v grep | awk '{ print $4 }' | xargs kill
+ps auwx | grep redis | grep -v grep
