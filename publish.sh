5d9e186716c4d5d0abccdfd1fd67fc34557005aa
Allan Brighton <Allan.Brighton@t-online.de>
2025-04-22 16:53:16 +0200

    Fixing jitpack build

--- publish.sh
+++ publish.sh
@@ -3,6 +3,8 @@
 # Note: This script is called from jitpack.yml.
 # The sbt version should be kept up to date with build.properties.
 
+eval `./coursier java --jvm temurin:1.21 --env`
+
 cd iris || exit
 
 java -Xms2048m -Xmx2048m -XX:ReservedCodeCacheSize=512m -jar ../sbt-launch-1.10.6.jar -Dsbt.color=false -Dprod.publish=true clean publishM2
