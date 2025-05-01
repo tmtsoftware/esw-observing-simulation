ee344c6c87a7eab3221832746d7d89e20cd587e9
Allan Brighton <Allan.Brighton@t-online.de>
2025-05-01 20:58:27 +0200

    Updated README.md, esw, csw version

--- simulation-scripts/versions.sh
+++ simulation-scripts/versions.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 #export ESW_VERSION=f9cbac5
-export ESW_VERSION=1.0.0
+export ESW_VERSION=v1.0.0
 export CSW_VERSION=6.0.0
 export SEQ_SCRIPT_VERSION=1a80475
 export TCS_VERSION=0.12.1
