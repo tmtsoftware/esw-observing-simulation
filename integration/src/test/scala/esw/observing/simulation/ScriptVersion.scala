04a6ddb154d36606aa4c408cda8290dd35f58958
Allan Brighton <Allan.Brighton@t-online.de>
2025-05-12 22:58:54 +0200

    Updated sequencer scripts version

--- integration/src/test/scala/esw/observing/simulation/ScriptVersion.scala
+++ integration/src/test/scala/esw/observing/simulation/ScriptVersion.scala
@@ -2,6 +2,5 @@ package esw.observing.simulation
 
 object ScriptVersion {
   // This is the version (or Git SHA) of the version of the sequencer-scripts repo to use
-//  val value = "119fd01"
-  val value = "v0.5.1"
+  val value = "119fd01"
 }
