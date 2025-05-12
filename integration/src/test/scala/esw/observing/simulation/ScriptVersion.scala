877875e81ce458eedd650c663c00cf692213528b
Allan Brighton <Allan.Brighton@t-online.de>
2025-05-12 22:31:11 +0200

    Updated GostConfig git SHA

--- integration/src/test/scala/esw/observing/simulation/ScriptVersion.scala
+++ integration/src/test/scala/esw/observing/simulation/ScriptVersion.scala
@@ -2,6 +2,6 @@ package esw.observing.simulation
 
 object ScriptVersion {
   // This is the version (or Git SHA) of the version of the sequencer-scripts repo to use
-  val value = "65bb415"
-//  val value = "0.1.0-SNAPSHOT"
+//  val value = "119fd01"
+  val value = "v0.5.1"
 }
