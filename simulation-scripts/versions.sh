44c4238d3e3a71b29b43e8ab3f946389a704ffbc
Allan Brighton <Allan.Brighton@t-online.de>
2025-05-12 23:30:23 +0200

    Updated sequencer scripts version

--- simulation-scripts/versions.sh
+++ simulation-scripts/versions.sh
@@ -3,7 +3,7 @@
 #export ESW_VERSION=f9cbac5
 export ESW_VERSION=v1.0.0
 export CSW_VERSION=6.0.0
-export SEQ_SCRIPT_VERSION=64ebac1
+export SEQ_SCRIPT_VERSION=4bcfc66
 export TCS_VERSION=0.12.1
 export ENG_UI_VERSION=1.0.0
 export CHANNEL="https://raw.githubusercontent.com/tmtsoftware/osw-apps/branch-6.0.x/apps.json"
